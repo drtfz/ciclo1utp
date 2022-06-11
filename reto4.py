51302248cc113e09696aebf35d1946ec05b0a5fc
Dave <davidduartef@gmail.com>
2022-06-11 17:20:10 -0500

    corrección reto 4

--- reto4.py
+++ reto4.py
@@ -1,18 +1,17 @@
-from typing_extensions import runtime
 
 
 def ordenes(rutinaContable):
     from functools import reduce
 
-    ordenMinima = 60000
+    ordenMinima = 600000
     ordenTotal = list(map(lambda x: [x[0]] + list(map(lambda y: y[1]*y[2], x[1:])), rutinaContable))
     ordenTotal = list(map(lambda x: [x[0]] + [reduce (lambda a,b:round(a+b,2), x[1:])], ordenTotal))
     ordenTotal = list(map(lambda x : x if x [1] >= ordenMinima else (x[0], x[1] + 25000), ordenTotal))
 
-    print('-----------------Inicio Registro Diario ----------------------')
+    print('------------------------ Inicio Registro diario ---------------------------------')
     for x in range (len(ordenTotal)):
-        print(f"la factura {ordenTotal[x][0]} tiene u total en pesos de {ordenTotal[x][1]:,.2f}")
-    print("-------------------Fin Registro Diario -------------------")
+        print(f"La factura {ordenTotal[x][0]} tiene un total en pesos de {ordenTotal[x][1]:,.2f}")
+    print("-------------------------- Fin Registro diario ----------------------------------")
 
 
 
